d8bcbcd120343c510787529893c9a7b70ed3578c
Mihai Versan <Mihaiv@imprezzio.com>
2018-02-26 16:13:36 +0200

    Initial Commit

---
+++ IntegrationDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IntegrationDemo
+//
+//  Created by Mihai Versan on 2/26/18.
+//  Copyright © 2018 SambaNetworks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
