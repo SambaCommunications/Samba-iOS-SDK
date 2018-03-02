f34b014fd4460bc037c62a5de065b0d890fe6d49
Mihai Versan <Mihaiv@imprezzio.com>
2018-03-02 09:48:18 +0200

    Updated SambaSDK + refactoring

--- IntegrationDemo/ViewController.swift
+++ IntegrationDemo/ViewController.swift
@@ -21,7 +21,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var errorLabel: UILabel!
     
     //  Keep an instance of Samba Ad Manager.
-    lazy var adManager = Samba.adManager!
+    var adManager : AdManager?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -60,32 +60,38 @@ class ViewController: UIViewController {
         //  After we got all the data we can configure Samba
         Samba.configure(setup: sambaSetup, videoConfig: videoConfig, target: target)
         
+        //  Initialize the ad manager
+        self.adManager = Samba.adManager
+        
         //  Set delegate in order to receive events (adDidLoad, adDidAppear, adDidReachEnd etc)
-        self.adManager.delegate = self
+        self.adManager?.delegate = self
         
         updateUI(secretKey: secretKey, publisherId: publisherId)
     }
     
     fileprivate func updateUI(secretKey: String, publisherId: Int) {
+        guard let adManager = self.adManager else {
+            return
+        }
+        
         self.secretKey.text = secretKey
         self.publisherId.text = String(publisherId)
         
         //  You should always check if ad is ready before showing it
-        showBtn.isEnabled = self.adManager.isReady
-        loadBtn.isEnabled = (!self.adManager.isLoading && !self.adManager.isReady)
+        showBtn.isEnabled = adManager.isReady
+        loadBtn.isEnabled = (!adManager.isLoading && !adManager.isReady)
     }
     
     @objc func loadButtonClicked() {
         loadBtn.isEnabled = false
         
         //  Call loadAd method when you want to receive content
-        self.adManager.loadAd()
+        self.adManager?.loadAd()
     }
 
     @objc func showButtonClicked() {
-        
         //  When content is ready you can call showAd method for showing the ad
-        self.adManager.showAd(from: self)
+        self.adManager?.showAd(from: self)
     }
     
     override func didReceiveMemoryWarning() {
@@ -99,26 +105,20 @@ extension ViewController: AdManagerProtocol {
     
     //  Method called when the Samba Ad finished loading
     func sambaAdDidLoad(_ adManager: AdManager) {
-        DispatchQueue.main.async {
-            self.errorLabel.text = ""
-            self.showBtn.isEnabled = true
-            self.loadBtn.isEnabled = false
-        }
+        self.errorLabel.text = ""
+        self.showBtn.isEnabled = true
+        self.loadBtn.isEnabled = false
     }
     
     func sambaAd(_ adManager: AdManager, didFailToLoad error: SambaError) {
-        DispatchQueue.main.async {
-            self.errorLabel.text = error.localizedDescription
-            self.loadBtn.isEnabled = true
-            self.showBtn.isEnabled = false
-        }
+        self.errorLabel.text = error.localizedDescription
+        self.loadBtn.isEnabled = true
+        self.showBtn.isEnabled = false
     }
     
     func sambaAdDidDisappear(_ adManager: AdManager) {
-        DispatchQueue.main.async {
-            self.loadBtn.isEnabled = true
-            self.showBtn.isEnabled = false
-        }
+        self.loadBtn.isEnabled = true
+        self.showBtn.isEnabled = false
     }
     
     func sambaAd(_ adManager: AdManager, didReachEnd adCompleted: Bool) {
@@ -127,11 +127,9 @@ extension ViewController: AdManagerProtocol {
     
     //  Method called when the user tries to play an ad with age restriction and he is under 18
     func ageRestrictionNotMet(_ adManager: AdManager) {
-        DispatchQueue.main.async {
-            self.errorLabel.text = "You must be over 18 to watch the ads."
-            self.showBtn.isEnabled = false
-            self.loadBtn.isEnabled = true
-        }
+        self.errorLabel.text = "You must be over 18 to watch the ads."
+        self.showBtn.isEnabled = false
+        self.loadBtn.isEnabled = true
     }
 }
 
